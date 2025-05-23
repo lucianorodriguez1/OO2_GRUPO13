737cd7790c1814e7efdf37f9d1320f4a2b10a63b
luciano <lucianorodriguez1@users.noreply.github.com>
2025-05-23 01:59:08 -0300

    primer commit

---
+++ src/main/java/com/oo2/grupo13/Grupo13Application.java
@@ -0,0 +1,13 @@
+package com.oo2.grupo13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo13Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/oo2/grupo13/Grupo13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oo2.grupo13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
