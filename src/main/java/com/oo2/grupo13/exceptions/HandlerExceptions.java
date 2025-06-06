da22e186368bbd85969487d351cb951ed36aa745
Camila Martello <martellocamilaa@gmail.com>
2025-06-06 01:15:34 -0300

    crear excepcion personzalizada para usuario con email ya registrado

--- src/main/java/com/oo2/grupo13/exceptions/HandlerExceptions.java
+++ src/main/java/com/oo2/grupo13/exceptions/HandlerExceptions.java
@@ -1,5 +1,17 @@
 package com.oo2.grupo13.exceptions;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
 public class HandlerExceptions {
+
+    @ExceptionHandler(EmailYaExisteException.class)
+    public ModelAndView manejarEmailYaExiste(EmailYaExisteException ex) {
+    	ModelAndView mAV = new ModelAndView("error/email-ya-existe");
+    	mAV.addObject("mensajeError", ex.getMessage());
+        return mAV;
+    }
 
 }
