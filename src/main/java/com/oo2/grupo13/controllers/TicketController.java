e502746642e3eeedc52791e96fec03a2031ed47d
savallarian45@gmail.com <savallarian45@gmail.com>
2025-06-08 18:51:00 -0300

    Agregados controller y vista

--- src/main/java/com/oo2/grupo13/controllers/TicketController.java
+++ src/main/java/com/oo2/grupo13/controllers/TicketController.java
@@ -2,14 +2,10 @@ package com.oo2.grupo13.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
-import com.oo2.grupo13.dto.TicketDTOCliente;
-import com.oo2.grupo13.dtos.SoporteDTO;
+import com.oo2.grupo13.dtos.TicketDTOCliente;
 import com.oo2.grupo13.services.ITicketService;
 
 @Controller
