684370e16e85b272f31c0a31a560abcec1442df7
savallarian45@gmail.com <savallarian45@gmail.com>
2025-06-09 02:05:57 -0300

    Agregados editar prioridad, estado y soporte para la vista de soportes de ticket

--- src/main/java/com/oo2/grupo13/controllers/TicketController.java
+++ src/main/java/com/oo2/grupo13/controllers/TicketController.java
@@ -1,7 +1,12 @@
 package com.oo2.grupo13.controllers;
 
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,19 +15,30 @@ import org.springframework.web.servlet.view.RedirectView;
 import com.oo2.grupo13.dtos.TareaDTO;
 import com.oo2.grupo13.dtos.TicketDTOCliente;
 import com.oo2.grupo13.dtos.TicketDTOSoporte;
+import com.oo2.grupo13.entities.Soporte;
+import com.oo2.grupo13.entities.Tarea;
+import com.oo2.grupo13.entities.Ticket;
+import com.oo2.grupo13.exceptions.TareaNoEncontradaException;
 import com.oo2.grupo13.helpers.ViewRouteHelper;
+import com.oo2.grupo13.services.ISoporteService;
 import com.oo2.grupo13.services.ITicketService;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @Controller
 @RequestMapping("/ticket")
 public class TicketController {
     private ITicketService ticketService;
+	private ISoporteService soporteService;
+    private ModelMapper modelMapper = new ModelMapper();
 
-    public TicketController(ITicketService ticketService) {
+    public TicketController(ITicketService ticketService, ISoporteService soporteService) {
+		this.soporteService = soporteService;
         this.ticketService = ticketService;
     }
 
-    
 	@GetMapping("/verTicketsCliente")
 	public ModelAndView verTicketsCliente() {
 		ModelAndView mAV = new ModelAndView("ticket/ver_tickets_cliente");
@@ -36,9 +52,36 @@ public class TicketController {
 		ModelAndView mAV = new ModelAndView("ticket/ver_tickets_soporte");
 		mAV.addObject("tickets", ticketService.getAll());
         mAV.addObject("ticket", new TicketDTOSoporte());
+		mAV.addObject("soportes", soporteService.getAll());
 		return mAV;
 	}
+	@GetMapping("/modificar/{id}")
+	public ModelAndView modificarTicket(@PathVariable("id") long id) {
+		ModelAndView mAV = new ModelAndView("ticket/editar_ticket");
+        TicketDTOSoporte ticket = ticketService.findByIdSoporte(id).get();
+        mAV.addObject("ticket", ticket);
+        mAV.addObject("soportes", soporteService.getAll());
+        return mAV;
+	}
 
+	@PostMapping("/editar")
+	public RedirectView editarTicket(@ModelAttribute("ticket") TicketDTOSoporte ticketDTO) {
+		Ticket ticketEditar = ticketService.findByIdSoporte(ticketDTO.getId()).map(ticket -> modelMapper.map(ticket, Ticket.class)).get();
+        ticketEditar.setEstado(ticketDTO.getEstado());
+        ticketEditar.setPrioridad(ticketDTO.getPrioridad());
+        
+        if (ticketDTO.getSoporteAsignado() != null && ticketDTO.getSoporteAsignado().getId() != 0) {
+            Soporte soporte = new Soporte();
+            soporte.setId(ticketDTO.getSoporteAsignado().getId());
+            ticketEditar.setSoporteAsignado(soporte);
+        } else {
+            ticketEditar.setSoporteAsignado(null); // Si no hay soporte, queda como null
+        }
+        ticketService.insertOrUpdateSoporte(modelMapper.map(ticketEditar, TicketDTOSoporte.class));
+        return new RedirectView(ViewRouteHelper.VER_TICKETS_SOPORTE);
+	}
+	
+	
 	@GetMapping("/nuevo")
 	public ModelAndView crearTicket() {
 		ModelAndView mAV = new ModelAndView("ticket/generar_ticket_cliente");
@@ -51,5 +94,6 @@ public class TicketController {
     ticketService.insertOrUpdateCliente(dto);
     return new RedirectView(ViewRouteHelper.VER_TICKETS_CLIENTE);
     }
+
 
 }
--- src/main/java/com/oo2/grupo13/services/ITicketService.java
+++ src/main/java/com/oo2/grupo13/services/ITicketService.java
@@ -1,6 +1,7 @@
 package com.oo2.grupo13.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.oo2.grupo13.dtos.TicketDTOCliente;
 import com.oo2.grupo13.dtos.TicketDTOSoporte;
@@ -9,6 +10,7 @@ import com.oo2.grupo13.entities.Ticket;
 public interface ITicketService {
     public List<Ticket> getAll();
     public Ticket findById(long id);
+    public Optional<TicketDTOSoporte> findByIdSoporte(long id);
     public TicketDTOCliente insertOrUpdateCliente(TicketDTOCliente ticketModel);
     public TicketDTOSoporte insertOrUpdateSoporte(TicketDTOSoporte ticketModel);
     public boolean remove(long id);
