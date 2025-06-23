30d7c34eb806056e7a0d5480d571ebc06e53c5e5
Camila Martello <martellocamilaa@gmail.com>
2025-06-23 13:54:50 -0300

    agregar areas al cliente

--- src/main/java/com/oo2/grupo13/dtos/ClienteEditarDTO.java
+++ src/main/java/com/oo2/grupo13/dtos/ClienteEditarDTO.java
@@ -1,5 +1,13 @@
 package com.oo2.grupo13.dtos;
 
-public class ClienteEditarDTO extends UsuarioEditarDTO {
+import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class ClienteEditarDTO extends UsuarioEditarDTO {
+	private List<Integer> areaIds;
 }
