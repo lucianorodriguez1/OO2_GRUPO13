b224f452607fe9a35f4489ea5935ca3f46ef261b
luciano <lucianorodriguez1@users.noreply.github.com>
2025-07-09 19:38:07 -0300

    cambios en soporte rest

--- src/main/java/com/oo2/grupo13/controllers/api/v1/SoporteRestController.java
+++ src/main/java/com/oo2/grupo13/controllers/api/v1/SoporteRestController.java
@@ -1,5 +1,6 @@
 package com.oo2.grupo13.controllers.api.v1;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.oo2.grupo13.dtos.SoporteDTO;
 import com.oo2.grupo13.entities.Soporte;
+import com.oo2.grupo13.entities.TURNO;
 import com.oo2.grupo13.repositories.ISoporteRepository;
 import com.oo2.grupo13.services.ISoporteService;
 
@@ -41,19 +43,35 @@ public class SoporteRestController {
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@PostMapping("/")
-	public ResponseEntity create(@RequestBody SoporteDTO soporte) {
-		if(soporte == null) return new ResponseEntity<String>("soporte does not exists", HttpStatus.BAD_REQUEST);
-		Optional<SoporteDTO> existente = soporteService.findByCuil(soporte.getCuil());
-		 if(existente.isPresent()) return new ResponseEntity<String>("duplicated document", HttpStatus.BAD_REQUEST);
+	public ResponseEntity create(@RequestBody SoporteRestDTO restDTO) {
+		if(restDTO == null) return new ResponseEntity<String>("soporte no existe", HttpStatus.BAD_REQUEST);
+		Optional<SoporteDTO> existente = soporteService.findByCuil(restDTO.cuil());
+		 if(existente.isPresent()) return new ResponseEntity<String>("documento con cuil duplicado", HttpStatus.BAD_REQUEST);
 		try {
-			soporteService.insertOrUpdate(modelMapper.map(soporte, SoporteDTO.class));
+	        SoporteDTO soporteDTO = new SoporteDTO();
+	        soporteDTO.setNombre(restDTO.nombre());
+	        soporteDTO.setApellido(restDTO.apellido());
+	        soporteDTO.setEmail(restDTO.email());
+	        soporteDTO.setPassword(restDTO.password());
+	        soporteDTO.setFotoPerfil(restDTO.fotoPerfil());
+	        soporteDTO.setCuil(restDTO.cuil());
+	        soporteDTO.setFechaIngreso(restDTO.fechaIngreso());
+	        soporteDTO.setTurno(restDTO.turno());
+	        SoporteDTO guardado = soporteService.insertOrUpdate(modelMapper.map(soporteDTO, SoporteDTO.class));
+	        return new ResponseEntity(modelMapper.map(guardado, SoporteDTO.class), HttpStatus.CREATED);
 		} catch (Exception e) {
 			return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		return new ResponseEntity(modelMapper.map(soporte, SoporteDTO.class), HttpStatus.CREATED);
 	}
 	
-	
-	
+	public record SoporteRestDTO(String nombre,
+		    String apellido,
+		    String email,
+		    String password,
+		    String fotoPerfil,
+		    String cuil,
+		    LocalDate fechaIngreso,
+		    TURNO turno) {}
+
 	
 }
