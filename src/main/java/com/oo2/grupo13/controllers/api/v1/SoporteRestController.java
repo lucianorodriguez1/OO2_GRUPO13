fbdde5dd3daa6af2bd86d7a2e9823e0a13c633bf
luciano <lucianorodriguez1@users.noreply.github.com>
2025-07-10 16:21:03 -0300

    mejorar anotacion del get y agregar una breve al del post de soporte

--- src/main/java/com/oo2/grupo13/controllers/api/v1/SoporteRestController.java
+++ src/main/java/com/oo2/grupo13/controllers/api/v1/SoporteRestController.java
@@ -24,7 +24,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema; 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+
 @RestController
 @RequestMapping("/api/v1/soporte")
 
@@ -32,60 +34,72 @@ public class SoporteRestController {
 	@Autowired
 	@Qualifier("soporteService")
 	private ISoporteService soporteService;
-	
+
 	private ModelMapper modelMapper = new ModelMapper();
 
-	//GET /api/v1/soporte 
+	// GET /api/v1/soporte
 	@GetMapping("/")
-	@Operation(
-			summary = "Obtener todos los soportes",
-			description = "Permite obtener una lista de todos los soportes registrados en el sistema"
-	)
+	@Operation(summary = "Obtener todos los soportes", description = "Permite obtener una lista de todos los soportes registrados en el sistema")
 	@ApiResponses(value = {
-			 @ApiResponse(responseCode = "200", description = "OK - Soportes cargados correctamente"),
-	         @ApiResponse(responseCode = "500", description = "ERROR - Internal server error",  content = @Content(
-	                 mediaType = "application/json",
-	                 schema = @Schema(implementation = String.class)
-	             ))
-	})
-	public ResponseEntity<List<SoporteDTO>> index(){
+			@ApiResponse(responseCode = "200", description = "OK - Lista de soportes", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = """
+					[
+					              {
+					                "id": 1,
+					                "nombre": "Juan",
+					                "apellido": "Pérez",
+					                "email": "juan@empresa.com",
+					                "fotoPerfil": null,
+					                "cuil": "20-12345678-9",
+					                "fechaIngreso": "2023-01-01",
+					                "turno": "TARDE",
+					                "rol": {
+										"id": 3,
+										"rol": "SOPORTE",
+										"fechaCreacion": "2025-06-20T13:10:59.478123",
+										"fechaActualizacion": "2025-06-20T13:10:59.478123"
+								},
+					                "especialidades": ["Redes", "Bases de datos"]
+					        	}
+					 ]"""))),
+			@ApiResponse(responseCode = "500", description = "ERROR - Internal server error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))) })
+	public ResponseEntity<List<SoporteDTO>> index() {
 		List<SoporteDTO> soportesDTO = new ArrayList<SoporteDTO>();
 		soportesDTO = soporteService.getAll();
-		
-		return new ResponseEntity<List<SoporteDTO>>(soportesDTO,HttpStatus.OK);
+
+		return new ResponseEntity<List<SoporteDTO>>(soportesDTO, HttpStatus.OK);
 	}
-	
+
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@PostMapping("/")
+	@Operation(
+		    summary = "Crear nuevo soporte",
+		    description = "Permite registrar un nuevo soporte en el sistema"
+		)
 	public ResponseEntity create(@RequestBody SoporteRestDTO restDTO) {
-		if(restDTO == null) return new ResponseEntity<String>("soporte no existe", HttpStatus.BAD_REQUEST);
+		if (restDTO == null)
+			return new ResponseEntity<String>("soporte no existe", HttpStatus.BAD_REQUEST);
 		Optional<SoporteDTO> existente = soporteService.findByCuil(restDTO.cuil());
-		 if(existente.isPresent()) return new ResponseEntity<String>("documento con cuil duplicado", HttpStatus.BAD_REQUEST);
+		if (existente.isPresent())
+			return new ResponseEntity<String>("documento con cuil duplicado", HttpStatus.BAD_REQUEST);
 		try {
-	        SoporteDTO soporteDTO = new SoporteDTO();
-	        soporteDTO.setNombre(restDTO.nombre());
-	        soporteDTO.setApellido(restDTO.apellido());
-	        soporteDTO.setEmail(restDTO.email());
-	        soporteDTO.setPassword(restDTO.password());
-	        soporteDTO.setFotoPerfil(restDTO.fotoPerfil());
-	        soporteDTO.setCuil(restDTO.cuil());
-	        soporteDTO.setFechaIngreso(restDTO.fechaIngreso());
-	        soporteDTO.setTurno(restDTO.turno());
-	        SoporteDTO guardado = soporteService.insertOrUpdate(modelMapper.map(soporteDTO, SoporteDTO.class));
-	        return new ResponseEntity(modelMapper.map(guardado, SoporteDTO.class), HttpStatus.CREATED);
+			SoporteDTO soporteDTO = new SoporteDTO();
+			soporteDTO.setNombre(restDTO.nombre());
+			soporteDTO.setApellido(restDTO.apellido());
+			soporteDTO.setEmail(restDTO.email());
+			soporteDTO.setPassword(restDTO.password());
+			soporteDTO.setFotoPerfil(restDTO.fotoPerfil());
+			soporteDTO.setCuil(restDTO.cuil());
+			soporteDTO.setFechaIngreso(restDTO.fechaIngreso());
+			soporteDTO.setTurno(restDTO.turno());
+			SoporteDTO guardado = soporteService.insertOrUpdate(modelMapper.map(soporteDTO, SoporteDTO.class));
+			return new ResponseEntity(modelMapper.map(guardado, SoporteDTO.class), HttpStatus.CREATED);
 		} catch (Exception e) {
 			return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
-	public record SoporteRestDTO(String nombre,
-		    String apellido,
-		    String email,
-		    String password,
-		    String fotoPerfil,
-		    String cuil,
-		    LocalDate fechaIngreso,
-		    TURNO turno) {}
 
-	
+	public record SoporteRestDTO(String nombre, String apellido, String email, String password, String fotoPerfil,
+			String cuil, LocalDate fechaIngreso, TURNO turno) {
+	}
+
 }
